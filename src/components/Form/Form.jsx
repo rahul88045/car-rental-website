6dc350bdff8f97801b60cf1ef65df157b46876c6
Sourav <sourav.kumar@falabella.cl>
2024-05-15 16:31:30 +0530

    Fixed Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,51 +3,50 @@ import React from "react";
 export default function Form() {
     return (
         <div className="container">
-            <form className="flex items-center ml-30">
-                <div className="space-y-12">
-                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div className="sm:col-span-4">
-                            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                                Name
-                            </label>
-                            <div className="mt-2">
-                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                    {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> */}
-                                    <input
-                                        type="text"
-                                        name="username"
-                                        id="username"
-                                        autoComplete="username"
-                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                        placeholder="janesmith"
-                                    />
-                                </div>
+            <form className="flex flex-col flex-grow ml-30">
+                <div className="mt-10">
+                    <div className="sm:col-span-4">
+                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                            Name
+                        </label>
+                        <div className="mt-2">
+                            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <input
+                                    type="text"
+                                    name="username"
+                                    id="username"
+                                    autoComplete="username"
+                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="janesmith"
+                                />
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div className="sm:col-span-4">
-                            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                                Phone Number
-                            </label>
-                            <div className="mt-2">
-                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                    <input
-                                        type="tel"
-                                        id="phone"
-                                        name="phone"
-                                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                        placeholder="janesmith"
-                                        required
-                                    />
-                                </div>
+                <div className="mt-10">
+                    <div className="sm:col-span-4">
+                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                            Phone Number
+                        </label>
+                        <div className="mt-2">
+                            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <input
+                                    type="tel"
+                                    id="phone"
+                                    name="phone"
+                                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="janesmith"
+                                    required
+                                />
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div className="sm:col-span-3">
+                <div className="mt-10">
+                    <div className="sm:col-span-4">
                         <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
                             Country
                         </label>
@@ -64,8 +63,10 @@ export default function Form() {
                             </select>
                         </div>
                     </div>
+                </div>
 
-                    <div className="sm:col-span-3">
+                <div className="mt-10">
+                    <div className="sm:col-span-4">
                         <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
                             From
                         </label>
@@ -85,7 +86,9 @@ export default function Form() {
                             </select>
                         </div>
                     </div>
+                </div>
 
+                <div className="mt-10">
                     <div className="sm:col-span-3">
                         <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
                             To
@@ -107,60 +110,62 @@ export default function Form() {
                         </div>
                     </div>
 
-                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div className="sm:col-span-4">
-                            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                                Name
-                            </label>
-                            <div className="mt-2">
-                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                    {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> */}
-                                    <input
-                                        type="date"
-                                        name="username"
-                                        id="username"
-                                        autoComplete="username"
-                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                        placeholder="janesmith"
-                                    />
-                                </div>
+                </div>
+
+
+
+                <div className="mt-10">
+                    <div className="sm:col-span-4">
+                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                            Date
+                        </label>
+                        <div className="mt-2">
+                            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> */}
+                                <input
+                                    type="date"
+                                    name="username"
+                                    id="username"
+                                    autoComplete="username"
+                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="janesmith"
+                                />
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div className="sm:col-span-4">
-                            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                                Name
-                            </label>
-                            <div className="mt-2">
-                                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                    {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> */}
-                                    <input
-                                        type="time"
-                                        name="username"
-                                        id="username"
-                                        autoComplete="username"
-                                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                        placeholder="janesmith"
-                                    />
-                                </div>
+                <div className="mt-10">
+                    <div className="sm:col-span-4">
+                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                            Time
+                        </label>
+                        <div className="mt-2">
+                            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> */}
+                                <input
+                                    type="time"
+                                    name="username"
+                                    id="username"
+                                    autoComplete="username"
+                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="janesmith"
+                                />
                             </div>
                         </div>
                     </div>
-
                 </div>
 
                 <div className="mt-6 flex items-center justify-end gap-x-6">
-                    {/* <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-          Cancel
-        </button>
-        <button
-          type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Save
-        </button> */}
+                    <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+                        Cancel
+                    </button>
+                    <button
+                        type="submit"
+                        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        Save
+                    </button>
                 </div>
             </form>
 
